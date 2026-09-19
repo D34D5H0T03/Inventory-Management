17a1f83226a4a45636401482f3ad98d27212a26e
Abdul Basit <abdulbasitraj03@gmail.com>
2026-09-19 07:18:54 +0600

    Added sample items to Database. Added Data serializer with REST framework. added Viewsets to handle HTTPS requests

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+#registering models for admin
+
+admin.site.register(Category)
+admin.site.register(Product)
